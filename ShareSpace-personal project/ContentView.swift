977b48781876e974d1c6b585e73b8139532a0bff
Collins Ren <feifanre@usc.edu>
2023-12-09 01:16:41 -0800

    Initial Commit

---
+++ ShareSpace-personal project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShareSpace-personal project
+//
+//  Created by 任非凡 on 12/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShareSpace-personal project/ShareSpace_personal_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareSpace_personal_projectApp.swift
+//  ShareSpace-personal project
+//
+//  Created by 任非凡 on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ShareSpace_personal_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
